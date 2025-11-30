ef630b079273f8ef91ce45243110d869667c1fbb
Akash Biswas <akashbiswas.271999@gmail.com>
2025-11-30 22:38:43 +0530

    Add shadcn components and create forms for auth.

---
+++ client/components/PasswordInputWithToggel.tsx
@@ -0,0 +1,38 @@
+"use state";
+import { Eye, EyeClosed } from "lucide-react";
+import { Input } from "./ui/input";
+import { ChangeEvent, useState } from "react";
+import { Button } from "./ui/button";
+export default function PasswordInputWithToggle({
+  onChange,
+  placeholder,
+}: {
+  placeholder: string;
+  onChange: (eve: ChangeEvent<HTMLInputElement>) => void;
+}) {
+  const [inputType, setInputType] = useState<"password" | "text">("password");
+
+  const updateInputType = () => {
+    if (inputType === "password") {
+      setInputType("text");
+    } else {
+      setInputType("password");
+    }
+  };
+  return (
+    <div className="w-full relative">
+      <Input type={inputType} onChange={onChange} placeholder={placeholder} />
+      <button
+        onClick={updateInputType}
+        type="button"
+        className="absolute top-1/2 right-2 cursor-pointer -translate-y-1/2"
+      >
+        {inputType === "password" ? (
+          <Eye onClick={updateInputType} />
+        ) : (
+          <EyeClosed onClick={updateInputType} />
+        )}
+      </button>
+    </div>
+  );
+}
